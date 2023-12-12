fa37688d99d87cc838ca4e719db79f9a0f9cd356
Quoc Thang <comehere.thang@gmail.com>
2023-12-12 09:38:29 +0700

    #57 update
    #57 update

--- Reactjs/src/containers/System/Admin/UserRedux.js
+++ Reactjs/src/containers/System/Admin/UserRedux.js
@@ -3,6 +3,7 @@ import {FormattedMessage} from 'react-intl'
 import {connect} from 'react-redux'
 import {getAllCodeService} from '../../../services/userService'
 import {LANGUAGES} from '../../../utils'
+import * as actions from '../../../store/actions'
 
 class UserRedux extends Component {
   constructor(props) {
@@ -13,16 +14,14 @@ class UserRedux extends Component {
   }
 
   async componentDidMount() {
-    try {
-      let res = await getAllCodeService('gender')
-      if (res && res.errCode === 0) {
-        this.setState({
-          genderArr: res.data
-        })
-      }
-      console.log('check res: ', res)
-    } catch (error) {
-      console.log('error: ', error)
+    this.props.getGenderStart()
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.genderRedux !== this.props.genderRedux) {
+      this.setState({
+        genderArr: this.props.genderRedux
+      })
     }
   }
 
@@ -142,12 +141,15 @@ class UserRedux extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    language: state.app.language
+    language: state.app.language,
+    genderRedux: state.admin.genders
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {}
+  return {
+    getGenderStart: () => dispatch(actions.fetchGenderStart())
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserRedux)
