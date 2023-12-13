7ae47f5e232bd928e595834194e1e6074c8af980
Quoc Thang <comehere.thang@gmail.com>
2023-12-13 13:10:28 +0700

    update
    update

--- Reactjs/src/containers/System/Admin/UserRedux.js
+++ Reactjs/src/containers/System/Admin/UserRedux.js
@@ -4,23 +4,27 @@ import {connect} from 'react-redux'
 import {LANGUAGES} from '../../../utils'
 import * as actions from '../../../store/actions'
 import './UserRedux.scss'
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css' // This only needs to be imported once in your app
 
 class UserRedux extends Component {
   constructor(props) {
     super(props)
     this.state = {
       genderArr: [],
-      roleArr: []
+      roleArr: [],
+      previewImgURL: '',
+      isOpen: false,
+
+      email: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      address: '',
+      role: '',
+      avatar: ''
     }
-
-    this.dropContainerRef = React.createRef()
-    this.fileInputRef = React.createRef()
-
-    // Bind the event handlers to the class instance
-    this.handleDragOver = this.handleDragOver.bind(this)
-    this.handleDragEnter = this.handleDragEnter.bind(this)
-    this.handleDragLeave = this.handleDragLeave.bind(this)
-    this.handleDrop = this.handleDrop.bind(this)
   }
 
   async componentDidMount() {
@@ -42,22 +46,35 @@ class UserRedux extends Component {
     }
   }
 
-  handleDragOver = (e) => {
-    e.preventDefault()
+  handleOnChangeImage = (event) => {
+    let data = event.target.files
+    let file = data[0]
+    if (file) {
+      let objectUrl = URL.createObjectURL(file)
+      this.setState({
+        previewImgURL: objectUrl
+      })
+    }
   }
 
-  handleDragEnter = () => {
-    this.dropContainerRef.current.classList.add('drag-active')
+  openPreviewImage = () => {
+    if (!this.state.previewImgURL) return
+    this.setState({
+      isOpen: true
+    })
   }
 
-  handleDragLeave = () => {
-    this.dropContainerRef.current.classList.remove('drag-active')
-  }
+  handleSaveUser = () => {}
 
-  handleDrop = (e) => {
-    e.preventDefault()
-    this.dropContainerRef.current.classList.remove('drag-active')
-    this.fileInputRef.current.files = e.dataTransfer.files
+  onChangeInput = () => {
+    // email: '',
+    //   password: '',
+    //   firstName: '',
+    //   lastName: '',
+    //   phoneNumber: '',
+    //   address: '',
+    //   role: '',
+    //   avatar: ''
   }
 
   render() {
@@ -65,7 +82,10 @@ class UserRedux extends Component {
     let roles = this.state.roleArr
     let language = this.props.language
     let isGetGenders = this.props.isLoadingGender
-    console.log('check props from component', this.state)
+
+    let {email, password, firstName, lastName, phoneNumber, address, role} =
+      this.state
+
     return (
       <div className="user-redux-container">
         <div className="title">MANAGE USERS REDUX</div>
@@ -79,116 +99,186 @@ class UserRedux extends Component {
                 <FormattedMessage id="manage-user.add" />
               </div>
 
-              <div className="col-6 my-3">
-                <label>
-                  <FormattedMessage id="manage-user.email" />
-                </label>
-                <input className="form-control" type="email"></input>
-              </div>
-
-              <div className="col-6 my-3">
-                <label>
-                  <FormattedMessage id="manage-user.password" />
-                </label>
-                <input className="form-control" type="password"></input>
-              </div>
-
-              <div className="col-3 my-2">
-                <label>
-                  <FormattedMessage id="manage-user.first-name" />
-                </label>
-                <input className="form-control" type="text"></input>
-              </div>
-
-              <div className="col-3 my-2">
-                <label>
-                  <FormattedMessage id="manage-user.last-name" />
-                </label>
-                <input className="form-control" type="text"></input>
-              </div>
-
-              <div className="col-2 my-2">
-                <label>
-                  <FormattedMessage id="manage-user.gender" />
-                </label>
-                <select className="form-control">
-                  {genders &&
-                    genders.length > 0 &&
-                    genders.map((item, index) => {
-                      return (
-                        <option key={index}>
-                          {language === LANGUAGES.VI
-                            ? item.valueVi
-                            : item.valueEn}
-                        </option>
-                      )
-                    })}
-                </select>
-              </div>
-
               <div className="col-4 my-2">
                 <label>
-                  <FormattedMessage id="manage-user.phone-number" />
+                  <FormattedMessage id="manage-user.image" />
                 </label>
-                <input className="form-control" type="text"></input>
-              </div>
-
-              <div className="col-9 my-2">
-                <label>
-                  <FormattedMessage id="manage-user.address" />
-                </label>
-                <input className="form-control" type="text"></input>
-              </div>
-
-              <div className="col-3 my-2">
-                <label>
-                  <FormattedMessage id="manage-user.role" />
-                </label>
-                <select className="form-control">
-                  {roles &&
-                    roles.length > 0 &&
-                    roles.map((item, index) => {
-                      return (
-                        <option key={index}>
-                          {language === LANGUAGES.VI
-                            ? item.valueVi
-                            : item.valueEn}
-                        </option>
-                      )
-                    })}
-                </select>
-              </div>
-
-              <div className="col-12">
-                <label
-                  className="drop-container"
-                  ref={this.dropContainerRef}
-                  onDragOver={this.handleDragOver}
-                  onDragEnter={this.handleDragEnter}
-                  onDragLeave={this.handleDragLeave}
-                  onDrop={this.handleDrop}
-                >
-                  <span className="drop-title">
-                    <FormattedMessage id="manage-user.image" />
-                  </span>
+                <div className="preview-img-container">
                   <input
+                    id="previewImg"
                     type="file"
-                    id="images"
+                    hidden
                     accept="image/*"
                     required
-                    ref={this.fileInputRef}
+                    onChange={(event) => this.handleOnChangeImage(event)}
                   ></input>
-                </label>
+                  <label className="label-upload" htmlFor="previewImg">
+                    <i className="fas fa-upload"></i>Chọn ảnh
+                  </label>
+                  <div
+                    className="preview-img"
+                    style={{
+                      backgroundImage: `url(${this.state.previewImgURL})`
+                    }}
+                    onClick={() => this.openPreviewImage()}
+                  ></div>
+                </div>
               </div>
 
-              <div className="col-12 mt-3">
-                <button className="btn btn-primary">
+              <div className="col-8">
+                <div className="row">
+                  <div className="col-12 my-2">
+                    <label>
+                      <FormattedMessage id="manage-user.email" />
+                    </label>
+                    <input
+                      className="form-control"
+                      type="email"
+                      value={email}
+                      onChange={(event) => {
+                        this.onChangeInput(event, 'email')
+                      }}
+                    ></input>
+                  </div>
+
+                  <div className="col-12 my-3">
+                    <label>
+                      <FormattedMessage id="manage-user.password" />
+                    </label>
+                    <input
+                      className="form-control"
+                      type="password"
+                      value={password}
+                      onChange={(event) => {
+                        this.onChangeInput(event, 'password')
+                      }}
+                    ></input>
+                  </div>
+
+                  <div className="col-4 my-2">
+                    <label>
+                      <FormattedMessage id="manage-user.first-name" />
+                    </label>
+                    <input
+                      className="form-control"
+                      type="text"
+                      value={firstName}
+                      onChange={(event) => {
+                        this.onChangeInput(event, 'firstName')
+                      }}
+                    ></input>
+                  </div>
+
+                  <div className="col-4 my-2">
+                    <label>
+                      <FormattedMessage id="manage-user.last-name" />
+                    </label>
+                    <input
+                      className="form-control"
+                      type="text"
+                      value={lastName}
+                      onChange={(event) => {
+                        this.onChangeInput(event, 'lastName')
+                      }}
+                    ></input>
+                  </div>
+
+                  <div className="col-4 my-2">
+                    <label>
+                      <FormattedMessage id="manage-user.gender" />
+                    </label>
+                    <select
+                      className="form-control"
+                      onChange={(event) => {
+                        this.onChangeInput(event, 'gender')
+                      }}
+                    >
+                      {genders &&
+                        genders.length > 0 &&
+                        genders.map((item, index) => {
+                          return (
+                            <option key={index}>
+                              {language === LANGUAGES.VI
+                                ? item.valueVi
+                                : item.valueEn}
+                            </option>
+                          )
+                        })}
+                    </select>
+                  </div>
+
+                  <div className="col-8 my-2">
+                    <label>
+                      <FormattedMessage id="manage-user.phone-number" />
+                    </label>
+                    <input
+                      className="form-control"
+                      type="text"
+                      value={phoneNumber}
+                      onChange={(event) => {
+                        this.onChangeInput(event, 'phoneNumber')
+                      }}
+                    ></input>
+                  </div>
+
+                  <div className="col-4 my-2">
+                    <label>
+                      <FormattedMessage id="manage-user.role" />
+                    </label>
+                    <select
+                      className="form-control"
+                      onChange={(event) => {
+                        this.onChangeInput(event, 'role')
+                      }}
+                    >
+                      {roles &&
+                        roles.length > 0 &&
+                        roles.map((item, index) => {
+                          return (
+                            <option key={index}>
+                              {language === LANGUAGES.VI
+                                ? item.valueVi
+                                : item.valueEn}
+                            </option>
+                          )
+                        })}
+                    </select>
+                  </div>
+
+                  <div className="col-12 my-2">
+                    <label>
+                      <FormattedMessage id="manage-user.address" />
+                    </label>
+                    <input
+                      className="form-control"
+                      type="text"
+                      value={address}
+                      onChange={(event) => {
+                        this.onChangeInput(event, 'address')
+                      }}
+                    ></input>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-12 mt-3 d-flex justify-content-center">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => this.handleSaveUser()}
+                >
                   <FormattedMessage id="manage-user.save" />
                 </button>
               </div>
             </div>
           </div>
         </div>
+
+        {this.state.isOpen === true && (
+          <Lightbox
+            mainSrc={this.state.previewImgURL}
+            onCloseRequest={() => this.setState({isOpen: false})}
+          />
+        )}
       </div>
     )
   }
